bf2e2141c1e8b53581bae54eec78bd09731569e9
ThisisAlam <103932559+ThisisAlam@users.noreply.github.com>
2026-04-30 20:48:51 +0500

    finalized the project.

---
+++ travel-spots-react-props-project/src/components/card-component/TravelCard.jsx
@@ -0,0 +1,24 @@
+import marker from "../../assets/marker.png"
+
+export default function TravelCard(props){
+
+    return(
+        <>
+            <div className="entry-card">
+                <img className="card-img"
+                src={props.entry.img.src} alt={props.entry.img.alt} />
+                <div className="card-info">
+                    <div className="location">
+                        <img src={marker} alt="marker logo image" />
+                        <p>{props.entry.country}</p>
+                        <a href={props.entry.googleMapsLink}>View on google maps</a>
+                    </div>
+                    <h1>{props.entry.title}</h1>
+                    <strong>{props.entry.dates}</strong>
+                    <p>{props.entry.text}</p>
+                </div>
+            </div>
+            <hr />
+        </>
+    )
+}
